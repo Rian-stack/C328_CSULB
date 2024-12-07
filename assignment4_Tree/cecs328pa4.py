b5edb98fcf3d850410883276949e7f6b4e386d48
Rianne Papa <paparianne728@gmail.com>
2024-12-06 19:30:30 -0800

    final

--- assignment4_Tree/cecs328pa4.py
+++ assignment4_Tree/cecs328pa4.py
@@ -1,23 +1,7 @@
+# Rian Papa
 import csv
 import networkx as nx
 import matplotlib.pyplot as plt
-
-def test_generate_mst():
-
-    # Create a graph
-    G = nx.Graph()
-    edges = [(0, 1, 10),
-        (0, 2, 6),
-        (0, 3, 5),
-        (1, 3, 15),
-        (2, 3, 4)]
-    G.add_weighted_edges_from(edges)
-
-    pos = nx.spring_layout(G)
-    
-    # Draw the graph
-    nx.draw(G, pos, with_labels=True)
-    plt.show()
 
 def generate_mst(file):
     edges = []
@@ -31,17 +15,14 @@ def generate_mst(file):
     G = nx.Graph()
     G.add_weighted_edges_from(edges)
 
-    # Draw MST
+    # Create MST
     mst = nx.minimum_spanning_tree(G, algorithm='kruskal')
     pos = nx.spring_layout(G)
 
     # Sum of edges of MST
     print (sum(weight for u,v,weight in mst.edges(data='weight')))
 
+    # Draw MST
     nx.draw(mst, pos, with_labels=True)
-    #nx.draw_networkx_edge_labels(mst, pos)
     nx.draw_networkx_edge_labels(mst, pos, edge_labels={(u, v): float(d['weight']) for u, v, d in mst.edges(data=True)})
     plt.show()
-
-#test_generate_mst()
-generate_mst('assignment4_Tree/test-input02.csv')
